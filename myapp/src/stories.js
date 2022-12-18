c62579ee184fff5226ec6d62e8786ef82521450b
Dilbar-ali <dilbarali0013@gmail.com>
2022-12-18 11:17:40 +0500

    Fetching Api complete

--- myapp/src/stories.js
+++ myapp/src/stories.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 function stories() {
+    let isloding=true;
+  let API = "http://hn.algolia.com/api/v1/search_by_date?query=html";
+
+  const fetcApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+     isloding=false;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    fetcApiData(API);
+  },[]);
+  if(isloding){ 
   return (
     <div>
-      <h1>stories</h1>
+      <h2>Loding ...</h2>
     </div>
-  )
+  );}
 }
 
-export default stories
+export default stories;
